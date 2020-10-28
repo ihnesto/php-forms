bb1832a912661dfb1deb1fd7a2d90a85cff7dd12
vasya <vasya@abc.com>
2020-10-28 19:42:25 +0200

    save to db added

--- send-feedback.php
+++ send-feedback.php
@@ -1,3 +1,24 @@
 <?php
 
-var_dump($_POST);
+//var_dump($_POST);
+
+$username = $_POST['username'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$link = mysqli_connect('localhost', 'root', '', 'feedback');
+
+$sql = "INSERT INTO items(username, subject, email, message, ip) VALUES(\"$username\", \"$subject\", \"$email\", \"$message\", \"$ip\")";
+
+//echo $sql;
+
+$ret = mysqli_query($link, $sql);
+if ($ret == false) {
+    echo mysqli_error($link);
+    exit();
+}
+mysqli_close($link);
+
+echo "<p>Thanks. Your feedback was saved.</p>";
